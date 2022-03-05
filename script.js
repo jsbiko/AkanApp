3d566ff5c0dcc34b8d507e70cf5082ee88437559
jsbiko <99875791+jsbiko@users.noreply.github.com>
2022-03-05 20:02:52 +0300

    initiated JS

---
+++ script.js
@@ -0,0 +1,10 @@
+// Our Akan Names based on DATE and GENDER inputs
+
+function myAkanName () {
+    let dayOfBirth = Number(document.getElementById("dayInput").value);
+    let monthOfBirth = Number(document.getElementById("monthInput").value);
+    let yearOfBirth = document.getElementById("yearInput").value;
+
+    let genders = document.getElementById("gender");
+
+}
